e15a943547e61bca34bd5178d040986cee3eb5c0
user.name : aaradhayasingh811 <user.email : aaradhayasingh811@gmail.com>
2025-01-10 00:04:21 +0530

    added overflow-hidden

--- src/component/Bottom.jsx
+++ src/component/Bottom.jsx
@@ -2,7 +2,7 @@ import React, { Children } from 'react'
 import Scale from './Scale'
 const Bottom = ({icon , text , children}) => {
   return (
-    <div className='flex flex-col h-80 w-full border-2 border-gray-200 my-2 rounded-2xl bg-white'>
+    <div className='flex flex-col h-80 w-full border-2 border-gray-200 my-2 rounded-2xl bg-white overflow-hidden'>
       <div className="flex gap-x-2 px-4 py-2 items-center justify-start text-slate-400">
       <div className="me-1">{icon}</div>
       <div className="">{text}</div>
